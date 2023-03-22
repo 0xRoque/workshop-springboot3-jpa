a44392e929cd197b6f823022ad2b420e9cb1ff9f
0xRoque <kodaksixx@gmail.com>
2023-03-22 17:02:12 +0000

    layer, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service 
+public class UserService {
+
+	// Declarar dependecia para o UserRepository
+
+	@Autowired
+	private UserRepository repository;
+
+	// Metodo para retornar todos os usuários do baco de Dados
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
