a85970e035550f46631111a9c9f6d312ff763e9e
0xRoque <kodaksixx@gmail.com>
2023-05-11 21:49:32 +0100

    Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service 
+public class CategoryService {
+
+	// Declarar dependecia para o CategoryRepository
+
+	@Autowired
+	private CategoryRepository repository;
+
+	// Metodo para retornar todos os usuários do baco de Dados
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
