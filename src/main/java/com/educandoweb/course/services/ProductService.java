f58910bee4995b3829910acf8337826c4b48aacf
0xRoque <kodaksixx@gmail.com>
2023-05-11 22:32:29 +0100

    Product

---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+@Service 
+public class ProductService {
+
+	// Declarar dependecia para o ProductRepository
+
+	@Autowired
+	private ProductRepository repository;
+
+	// Metodo para retornar todos os usuários do baco de Dados
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
